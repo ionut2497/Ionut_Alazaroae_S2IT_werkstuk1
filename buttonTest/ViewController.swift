afe178d9dc978c15a6cd2bced670df257951b5cc
student <student@dt-vm-osx.local>
2018-05-31 22:01:35 +0200

    Test commit

--- buttonTest/ViewController.swift
+++ buttonTest/ViewController.swift
@@ -9,16 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttonPushed(_ sender: UIButton) {
+        let title = sender.title(for:.normal)
+        myLabel.text = "you clicked \(title!) "
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
+    
 
 
 }
